029e92847bd5d851b698f13120298a0f10d89397
Julia <jkolupaeva@gmail.com>
2022-10-29 19:33:54 +0400

    feat: images download

--- __tests__/page-loader.test.js
+++ __tests__/page-loader.test.js
@@ -1,25 +1,58 @@
-import * as fs from 'node:fs/promises';
+import * as fsp from 'node:fs/promises';
 import os from 'os';
-import path from 'path';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
 import nock from 'nock';
 import pageLoader from '../src/index';
+import { response, expected } from '../__fixtures__/html';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const getFixturePath = (filename) => path.resolve(__dirname, '..', '__fixtures__', filename);
 
 const url = 'https://ru.hexlet.io/courses';
-const expectedData = '<!DOCTYPE html><html><head></head><body></body></html>';
 const filename = 'ru-hexlet-io-courses.html';
+const directoryName = 'ru-hexlet-io-courses_files';
+const imageNames = [
+  'ru-hexlet-io-assets-professions-nodejs.png',
+  'ru-hexlet-io-assets-professions-nodejs2.png',
+];
 let tempDir;
+let images = [];
 
 // beforeAll();
 beforeEach(async () => {
-  tempDir = await fs.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
+  tempDir = await fsp.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
+  const image1 = await fsp.readFile(getFixturePath('image1.png'), null);
+  const image2 = await fsp.readFile(getFixturePath('image2.png'), null);
+  images = [image1, image2];
+  nock('https://ru.hexlet.io').get('/courses').reply(200, response);
+  nock('https://ru.hexlet.io').get('/assets/professions/nodejs.png').reply(200, image1);
+  nock('https://ru.hexlet.io').get('/assets/professions/nodejs2.png').reply(200, image2);
 });
 
 test('page download', async () => {
-  nock('https://ru.hexlet.io').get('/courses').reply(200, expectedData);
-
   const result = await pageLoader(url, tempDir);
-  const fileData = await fs.readFile(path.join(tempDir, filename), { encoding: 'utf8' });
+  const files = await fsp.readdir(tempDir);
 
   expect(result).toBe(path.join(tempDir, filename));
-  expect(fileData).toEqual(expectedData);
+  expect(files).toContain(filename);
+});
+
+test('html is correct', async () => {
+  await pageLoader(url, tempDir);
+  const fileData = await fsp.readFile(path.join(tempDir, filename), { encoding: 'utf8' });
+  expect(fileData).toEqual(expected);
+});
+
+test('images downloaded', async () => {
+  await pageLoader(url, tempDir);
+  const directory = path.join(tempDir, directoryName);
+  const files = await fsp.readdir(directory);
+
+  imageNames.forEach(async (name, i) => {
+    expect(files).toContain(name);
+    const tempFile = await fsp.readFile(path.join(directory, name), null);
+    expect(tempFile).toEqual(images[i]);
+  });
 });
--- src/index.js
+++ src/index.js
@@ -1,22 +1,78 @@
-import * as fs from 'node:fs/promises';
+import { load } from 'cheerio';
+import * as fsp from 'node:fs/promises';
 import path from 'path';
 import axios from 'axios';
 
-const getFilename = (address) => {
-  const extension = '.html';
-  const url = new URL(address);
-  const regex = /[^A-Z0-9]+/gi;
-  const addressNormalized = (url.hostname + url.pathname).replace(regex, '-');
-  return addressNormalized + extension;
+const isAbsoluteURL = (string) => {
+  const regex = /^(?:[a-z+]+:)?\/\//i;
+  return regex.test(string);
 };
 
+const normalizeString = (string) => {
+  const regex = /[^A-Z0-9]+/gi;
+  return string.replace(regex, '-');
+};
+
+const getNames = (url) => {
+  const address = normalizeString(url.hostname + url.pathname);
+  return [`${address}.html`, `${address}_files`];
+};
+
+const normalizeLink = (link, url) => {
+  const extensionIndex = link.lastIndexOf('.');
+  const body = link.slice(0, extensionIndex);
+  const extension = link.slice(extensionIndex);
+  return normalizeString(`${url.hostname}-${body}`) + extension;
+};
+
+const replaceImageLinks = ($, dirname, normalizedLinks) => {
+  $('img').each(function (i) {
+    const newSrc = `${dirname}/${normalizedLinks[i]}`;
+    $(this).attr('src', newSrc);
+  });
+  return $.html();
+};
+
+const loadImages = (url, directory, links, normalizedLinks) => {
+  const promises = links.map((link, i) => {
+    const imageLink = `${isAbsoluteURL(link) ? '' : `${url.protocol}//${url.hostname}`}${link}`;
+    return axios({
+      method: 'get',
+      responseType: 'stream',
+      url: imageLink,
+    })
+      .then((res) => {
+        const filepath = path.join(directory, normalizedLinks[i]);
+        return fsp.writeFile(filepath, res.data);
+      })
+      .catch(console.log);
+  });
+  return promises;
+};
+
+const getImageLinks = ($) => $('img')
+  .map(function () {
+    return $(this).attr('src');
+  })
+  .toArray();
+
 const pageLoader = (address, directory) => {
-  const filename = getFilename(address);
-  const pathname = path.join(directory, filename);
-  return axios
-    .get(address)
-    .then((response) => fs.writeFile(pathname, response.data))
-    .then(() => pathname)
+  const url = new URL(address);
+  const [filename, dirname] = getNames(url);
+  const assetsDirPath = path.join(directory, dirname);
+  const pagePath = path.join(directory, filename);
+
+  return Promise.all([axios.get(address), fsp.mkdir(assetsDirPath)])
+    .then(([response]) => {
+      const html = response.data;
+      const $ = load(html);
+      const links = getImageLinks($);
+      const normalizedLinks = links.map((link) => normalizeLink(link, url));
+      const newHtml = replaceImageLinks($, dirname, normalizedLinks);
+      const promises = loadImages(url, assetsDirPath, links, normalizedLinks);
+      return Promise.all([fsp.writeFile(pagePath, newHtml), ...promises]);
+    })
+    .then(() => pagePath)
     .catch(console.log);
 };
 
