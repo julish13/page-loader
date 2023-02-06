137584d332cb3310a65b098911f77efc184bb8cc
Julia <jkolupaeva@gmail.com>
2023-02-06 20:15:23 +0400

    fix: actions fail

--- bin/page-loader.js
+++ bin/page-loader.js
@@ -1,13 +1,12 @@
 #!/usr/bin/env node
 
-import os from 'os';
 import { program } from 'commander';
 import pageLoader from '../src/index.js';
 
 program
   .version('1.0.0')
   .description('Page loader utility')
-  .option('-o --output [dir]', 'output dir', os.homedir())
+  .option('-o --output [dir]', 'output dir')
   .arguments('<url>')
   .action((url) => {
     pageLoader(url, program.opts().output).then(console.log);
