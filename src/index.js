6aabf82c5313d6a958cbc1c3de387486d6030631
Julia <jkolupaeva@gmail.com>
2023-02-15 22:21:01 +0400

    fix: set current dir as default

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { createRequire } from 'module';
 import { load } from 'cheerio';
 import * as fsp from 'node:fs/promises';
 import path from 'path';
+import process from 'process';
 import debug from 'debug';
 import {
   getAssetsNames, getLinks, replaceLinks, processAssets,
@@ -12,10 +13,11 @@ require('axios-debug-log');
 const axios = require('axios');
 
 const log = debug('page-loader');
+const currentDir = process.cwd();
 
-const pageLoader = (address, directory) => {
+const pageLoader = (address, directory = currentDir) => {
   const url = new URL(address);
-  log('preparing paths for the page and its assets');
+  log(`preparing paths in ${directory} for the page ${address} and its assets`);
   const { filename, dirname } = getAssetsNames(url);
   const assetsDirPath = path.join(directory, dirname);
   const pagePath = path.join(directory, filename);
