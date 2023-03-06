c13f0411f7f02df83869773fa12e5872adbcced6
Julia <jkolupaeva@gmail.com>
2023-03-06 21:01:47 +0400

    fix: default dir in bin file

--- bin/page-loader.js
+++ bin/page-loader.js
@@ -4,10 +4,12 @@ import debug from 'debug';
 import { program } from 'commander';
 import pageLoader from '../src/index.js';
 
+const currentDir = process.cwd();
+
 program
   .version('1.0.0')
   .description('Page loader utility')
-  .option('-o --output [dir]', `output dir (default: ${process.cwd()})`)
+  .option('-o --output [dir]', `output dir (default: ${currentDir})`, currentDir)
   .option('-d --debug', 'enables debug logger')
   .arguments('<url>')
 
